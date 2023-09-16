d62f1c49e3d147fa145ef1fb6dc6d0686c4c1044
Nika Kakhiani <nikakakhi@gmail.com>
2023-09-16 17:21:34 +0400

    worldwise app

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import styles from "./CityItem.module.css";
+import PropTypes from "prop-types";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { cityName, emoji, date, id, position } = city;
+
+  return (
+    <li className={styles.cityItem}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
+    </li>
+  );
+}
+
+CityItem.propTypes = {
+  city: PropTypes.shape({
+    cityName: PropTypes.string.isRequired,
+    emoji: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    position: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default CityItem;
